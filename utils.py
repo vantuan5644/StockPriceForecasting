345aa84cef4b7867757ef4698b14157dc95c06c6
Tuan Tran <vantuan5644@gmail.com>
2020-12-27 11:49:16 +0700

    Update notebooks, added reports

--- utils.py
+++ utils.py
@@ -1,3 +1,7 @@
+from sktime.forecasting.theta import ThetaForecaster
+from statsmodels.tsa.arima_model import ARIMA
+
+
 def get_data_splits(df, train_ratio: float, validation_ratio: float, test_ratio: float):
     train_len = int(df.shape[0] * train_ratio)
     train_data = df.iloc[: train_len]
@@ -5,3 +9,4 @@ def get_data_splits(df, train_ratio: float, validation_ratio: float, test_ratio:
     test_data = df.iloc[-test_len:]
     val_data = df.iloc[train_len:-test_len]
     return train_data, val_data, test_data
+
